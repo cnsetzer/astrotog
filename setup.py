f07c2def1a3dc9b5717ca501fbf6543013528639
Christian <aerosetzer@gmail.com>
2018-08-22 15:17:22 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ ext = Extension(name='macronova2py',
 if __name__ == "__main__":
 
     setup(name='astrotog',
-          version='0.1.5',
+          version='0.1.6',
           description='Functions for generating mock observations of astrophysical transients',
           url='http://github.com/cnsetzer/astrotog',
           author='Christian Setzer',
