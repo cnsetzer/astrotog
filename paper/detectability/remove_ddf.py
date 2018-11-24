5735503c479e08d2a8ae3db9cbbd99b1287be996
Christian <aerosetzer@gmail.com>
2018-11-25 00:33:48 +0100

    Add filternull flag for alt_sched
    also add flags to skip outputs if they've all been done before

--- paper/detectability/remove_ddf.py
+++ paper/detectability/remove_ddf.py
@@ -8,114 +8,122 @@ cad_path = '/share/data1/csetzer/lsst_cadences/'
 sims = os.listdir(results_folders)
 
 for sim in sims:
-    print('\nProcessing wfd/ddf separation for {}'.format(sim))
-    if re.search('kraken2026',sim):
-        ddf_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'kraken_2026.db',
-            subset='ddf',
-            opsimversion='lsstv4',
-        ).summary
-        wfd_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'kraken_2026.db',
-            subset='wfd',
-            opsimversion='lsstv4',
-        ).summary
-    if re.search('alt',sim):
-        ddf_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'alt_sched_rolling.db',
-            subset='ddf',
-            opsimversion='sstf',
-        ).summary
-        wfd_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'alt_sched_rolling.db',
-            subset='wfd',
-            opsimversion='sstf',
-        ).summary
-    if re.search('kraken2042',sim):
-        ddf_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'kraken_2042.db',
-            subset='ddf',
-            opsimversion='lsstv4',
-        ).summary
-        wfd_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'kraken_2042.db',
-            subset='wfd',
-            opsimversion='lsstv4',
-        ).summary
-    if re.search('nexus',sim):
-        ddf_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'nexus_2097.db',
-            subset='ddf',
-            opsimversion='lsstv4',
-        ).summary
-        wfd_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'nexus_2097.db',
-            subset='wfd',
-            opsimversion='lsstv4',
-        ).summary
-    if re.search('pontus2002',sim):
-        ddf_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'pontus_2002.db',
-            subset='ddf',
-            opsimversion='lsstv4',
-        ).summary
-        wfd_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'pontus_2002.db',
-            subset='wfd',
-            opsimversion='lsstv4',
-        ).summary
-    if re.search('pontus2489',sim):
-        ddf_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'pontus_2489.db',
-            subset='ddf',
-            opsimversion='lsstv4',
-        ).summary
-        wfd_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'pontus_2489.db',
-            subset='wfd',
-            opsimversion='lsstv4',
-        ).summary
-    if re.search('pontus2573',sim):
-        ddf_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'pontus_2573.db',
-            subset='ddf',
-            opsimversion='lsstv4',
-        ).summary
-        wfd_obs = oss.OpSimOutput.fromOpSimDB(
-            cad_path + 'pontus_2573.db',
-            subset='wfd',
-            opsimversion='lsstv4',
-        ).summary
-
     sim_outs = os.listdir(results_folders+sim+'/')
-
+    done = 0
     for prod in sim_outs:
-        ddf_ind = []
-        wfd_ind = []
         if re.search(r'wfd|ddf', prod):
-            continue
-        if ('wfd_'+prod in sim_outs) and ('ddf_'+prod in sim_outs):
-            continue
-        if re.search('detections', prod):
-            print('\nDoing separation for {}'.format(prod))
-            file = pd.read_csv(results_folders+sim+'/'+prod, index_col=0)
-            if 'mjd' in file.columns:
-                for index, series in file.iterrows():
-                    obsmjd = series['mjd']
-                    qry = ddf_obs.query('expMJD == {}'.format(obsmjd))
-                    qry2 = wfd_obs.query('expMJD == {}'.format(obsmjd))
-                    if qry.empty:
-                        pass
-                    else:
-                        ddf_ind.append(index)
-                    if qry2.empty:
-                        pass
-                    else:
-                        wfd_ind.append(index)
+            done += 1
+    if done == 16:
+        continue
+    else:
+        print('\nProcessing wfd/ddf separation for {}'.format(sim))
+        if re.search('kraken2026',sim):
+            ddf_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'kraken_2026.db',
+                subset='ddf',
+                opsimversion='lsstv4',
+            ).summary
+            wfd_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'kraken_2026.db',
+                subset='wfd',
+                opsimversion='lsstv4',
+            ).summary
+        if re.search('alt',sim):
+            ddf_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'alt_sched_rolling.db',
+                subset='ddf',
+                opsimversion='sstf',
+                filterNull=True,
+            ).summary
+            wfd_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'alt_sched_rolling.db',
+                subset='wfd',
+                opsimversion='sstf',
+                filterNull=True,
+            ).summary
+        if re.search('kraken2042',sim):
+            ddf_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'kraken_2042.db',
+                subset='ddf',
+                opsimversion='lsstv4',
+            ).summary
+            wfd_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'kraken_2042.db',
+                subset='wfd',
+                opsimversion='lsstv4',
+            ).summary
+        if re.search('nexus',sim):
+            ddf_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'nexus_2097.db',
+                subset='ddf',
+                opsimversion='lsstv4',
+            ).summary
+            wfd_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'nexus_2097.db',
+                subset='wfd',
+                opsimversion='lsstv4',
+            ).summary
+        if re.search('pontus2002',sim):
+            ddf_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'pontus_2002.db',
+                subset='ddf',
+                opsimversion='lsstv4',
+            ).summary
+            wfd_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'pontus_2002.db',
+                subset='wfd',
+                opsimversion='lsstv4',
+            ).summary
+        if re.search('pontus2489',sim):
+            ddf_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'pontus_2489.db',
+                subset='ddf',
+                opsimversion='lsstv4',
+            ).summary
+            wfd_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'pontus_2489.db',
+                subset='wfd',
+                opsimversion='lsstv4',
+            ).summary
+        if re.search('pontus2573',sim):
+            ddf_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'pontus_2573.db',
+                subset='ddf',
+                opsimversion='lsstv4',
+            ).summary
+            wfd_obs = oss.OpSimOutput.fromOpSimDB(
+                cad_path + 'pontus_2573.db',
+                subset='wfd',
+                opsimversion='lsstv4',
+            ).summary
+
+        for prod in sim_outs:
+            ddf_ind = []
+            wfd_ind = []
+            if re.search(r'wfd|ddf', prod):
+                continue
+            if ('wfd_'+prod in sim_outs) and ('ddf_'+prod in sim_outs):
+                continue
+            if re.search('detections', prod):
+                print('\nDoing separation for {}'.format(prod))
+                file = pd.read_csv(results_folders+sim+'/'+prod, index_col=0)
+                if 'mjd' in file.columns:
+                    for index, series in file.iterrows():
+                        obsmjd = series['mjd']
+                        qry = ddf_obs.query('expMJD == {}'.format(obsmjd))
+                        qry2 = wfd_obs.query('expMJD == {}'.format(obsmjd))
+                        if qry.empty:
+                            pass
+                        else:
+                            ddf_ind.append(index)
+                        if qry2.empty:
+                            pass
+                        else:
+                            wfd_ind.append(index)
+                else:
+                    continue
             else:
                 continue
-        else:
-            continue
 
         wfd_ver = file[file.index.isin(wfd_ind)]
         ddf_ver = file[file.index.isin(ddf_ind)]
